094fdc3fc52ba4a6c364a6ad4a2c35bf97362ae0
Vitaly Voskobovich <raficone@gmail.com>
2015-07-13 12:20:06 +0300

    Init

---
+++ behaviors/NestedSetsBehavior.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace voskobovich\nestedsets\behaviors;
+
+/**
+ * Class NestedSetsBehavior
+ * @package voskobovich\nestedsets\behaviors
+ */
+class NestedSetsBehavior extends \creocoder\nestedsets\NestedSetsBehavior
+{
+
+}
---
+++ widgets/Nestable.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace voskobovich\nestedsets\widgets;
+
+/**
+ * Class Nestable
+ * @package voskobovich\nestedsets\widgets
+ */
+class Nestable extends \yii\base\Widget
+{
+
+}
